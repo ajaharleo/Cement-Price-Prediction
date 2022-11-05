abf6539823859998d35dc739fd9e128d2a9cee72
ajaharleo <ajaharuddin10217@gmail.com>
2022-11-05 11:11:07 +0530

    constants for data ingestion and data valiation

--- src/CementStrength/constants/__init__.py
+++ src/CementStrength/constants/__init__.py
@@ -1,4 +1,36 @@
 from pathlib import Path
+from datetime import datetime
+import os
 
 CONFIG_FILE_PATH = Path("configs/config.yaml")
-#PARAMS_FILE_PATH = Path("params.yaml")
+ROOT_DIR = os.getcwd()
+
+
+def get_current_time_stamp():
+    return f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
+
+#Data ingestion related variables
+DATA_INGESTION_CONFIG_KEY = "data_ingestion_config"
+DATA_INGESTION_ARTIFACT_DIR = "data_ingestion"
+DATA_INGESTION_DOWNLOAD_URL_KEY = "dataset_download_url"
+DATA_INGESTION_RAW_DATA_DIR_KEY = "raw_data_dir"
+DATA_INGESTION_TGZ_DOWNLOAD_DIR_KEY = "tgz_download_dir"
+DATA_INGESTION_INGESTED_DIR_NAME_KEY = "ingested_dir"
+DATA_INGESTION_TRAIN_DIR_KEY = "ingested_train_dir"
+DATA_INGESTION_TEST_DIR_KEY = "ingested_test_dir"
+
+#Data Validation related variables
+DATA_VALIDATION_CONFIG_KEY = 'data_validation_config'
+DATA_VALIDATION_ARTIFACT_DIR = "data_validation"
+DATA_VALIDATION_SCHEMA_FILE_NAME_KEY = "schema_file_name"
+DATA_VALIDATION_SCHEMA_DIR_KEY = 'schema_dir'
+DATA_VALIDATION_REPORT_FILE_NAME_KEY = 'report_file_name'
+DATA_VALIDATION_REPORT_PAGE_FILE_NAME_KEY = 'report_page_file_name'
+SCHEMA_COLUMNS_KEY = 'columns'
+SCHEMA_NUMERICAL_COLUMNS_KEY = 'numerical_columns'
+SCHEMA_CATEGORICAL_COLUMNS_KEY = 'categorical_columns'
+SCHEMA_TARGET_COLUMN_KEY = 'target_column'
+SCHEMA_DOMAIN_VALUE_KEY = 'domain_value'
+
+EXPERIMENT_DIR_NAME="experiment"
+EXPERIMENT_FILE_NAME="experiment.csv"
