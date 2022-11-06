447ef539af12b20afaf103ff134e9819e4df8683
ajaharleo <ajaharuddin10217@gmail.com>
2022-11-06 20:07:13 +0530

    model trainer constants added

--- src/CementStrength/constants/__init__.py
+++ src/CementStrength/constants/__init__.py
@@ -43,5 +43,14 @@ DATA_TRANSFORMATION_PREPROCESSING_DIR_KEY = "preprocessing_dir"
 DATA_TRANSFORMATION_PREPROCESSED_OBJECT_FILE_NAME_KEY = "preprocessed_object_file_name"
 DATASET_SCHEMA_COLUMNS_KEY = "columns"
 
+#Model trainer related variables
+MODEL_TRAINER_CONFIG_KEY = 'model_trainer_config'
+MODEL_TRAINER_ARTIFACT_DIR = 'model_trainer'
+MODEL_TRAINER_TRAINED_MODEL_DIR_KEY  = 'trained_model_dir'
+MODEL_TRAINER_MODEL_FILE_NAME_KEY = 'model_file_name'
+MODEL_TRAINER_BASE_ACCURACY_KEY = 'base_accuracy'
+MODEL_TRAINER_MODEL_CONFIG_DIR_NAME_KEY = 'model_config_dir'
+MODEL_TRAINER_MODEL_CONFIG_FILE_NAME_KEY = 'model_config_file_name'
+
 EXPERIMENT_DIR_NAME="experiment"
 EXPERIMENT_FILE_NAME="experiment.csv"
