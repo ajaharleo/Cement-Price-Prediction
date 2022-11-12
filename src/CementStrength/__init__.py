c88e879b9897552c1915d5725ff4ce9f3424c5f5
ajaharleo <ajaharuddin10217@gmail.com>
2022-11-13 03:59:05 +0530

    final flask app data

--- src/CementStrength/__init__.py
+++ src/CementStrength/__init__.py
@@ -2,6 +2,7 @@ import os,sys
 import logging
 from CementStrength.constants import *
 
+
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s:%(lineno)d:%(filename)s:%(funcName)s]: %(message)s"
 log_dir = "logs"
 log_filepath = os.path.join(log_dir, f"log_{get_current_time_stamp()}.log")
@@ -16,3 +17,17 @@ logging.basicConfig(
     ])
 
 logger = logging.getLogger("CementStrengthLogger")
+
+def get_log_dataframe(file_path:Path):
+    data=[]
+    with open(file_path) as log_file:
+        for line in log_file.readlines():
+            data.append(line.split("^;"))
+
+    log_df = pd.DataFrame(data)
+    columns=["Time stamp","Log Level","Module","line number","file name","function name","message"]
+    log_df.columns=columns
+    
+    log_df["log_message"] = log_df['Time stamp'].astype(str) +":$"+ log_df["message"]
+
+    return log_df[["log_message"]]
